e274ffa65b0e2c0d24d6758622779eb4e95b2dba
MDN <mark.denardo@gmail.com>
2022-12-14 09:26:27 -0500

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,19 @@
+//  A 'first steps in python' course, made by GIANT Room Innovator MDN 2022  
+
+//  Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.
+
+//  https://en.wikipedia.org/wiki/Python_(programming_language)
+
+  print("Hello Humans, can't wait to code w y'all.")
+
+//  print() function works like a printer. Put the data, or argument within the parenthese and it will print when you compile your python file:
+
+// try using the print() function in a compiler https://www.programiz.com/python-programming/online-compiler/
+
+x = 0
+y = 1          // int, float, complex
+n = "python"  //  str
+b = True      //  bool
+
+// here are some data types, you may notice that the string data type has quotes
+
