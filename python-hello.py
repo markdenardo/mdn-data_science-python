4063928cce3ea588febf00fb5fab22e4eb12fa19
MDN <mark.denardo@gmail.com>
2023-01-31 22:19:45 -0500

    Create python-hello.py

---
+++ python-hello.py
@@ -0,0 +1,34 @@
+#  how to print 
+print("hi")
+#  this is how you comment out code and take notes
+#  variables:
+#  python has no command for variable assignment
+x=10
+print(x)
+y=-10
+print(y)
+# data types
+# some data types are : str int bool
+#  str
+word="word"
+print(word)
+print(type(word))
+#  int
+number = 10
+print(number)
+print(type(number))
+#  bool
+value = True
+print(value)
+print(type(value))
+#  arithmetic operations
+print(1+1)
+print(100-1)
+print(10*10)
+print(100/10)
+print(100%10)
+#  assignment: comment out all code about this line
+#  assignment: print a few sentences
+#  assignment: answer the question, what data type is True
+#  assignment: pick a random number and try to get to that number through arithmetic(basic algebra)
+
